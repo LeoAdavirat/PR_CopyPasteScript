62f242e645db028d05fb480123fc03f45a46372e
LeoAdavirat <52106425+LeoAdavirat@users.noreply.github.com>
2023-02-25 14:58:10 +0700

    Latest script v1.6
    Changelog:
    V1.6 - Added an exception

--- SCRIPT_HTTT_COPYPASTE.py
+++ SCRIPT_HTTT_COPYPASTE.py
@@ -1,20 +1,21 @@
 #Import libraries
 import pyperclip
 
-#Input Page name
-Page_name = input("Please input page name: ")
+#Prepare List
+List_replace_Key = ["{}", "(TÊN PAGE)", "(PAGE'S NAME)", "(PAGE's NAME)", "(full name of the club/Project)", "(full name Club/Project)", "(tên đầy đủ của CLB/Dự án)", " (tên đầy đủ CLB/Dự án)"]
 
-#Read the script
-with open("SCRIPT_HTTT.txt", encoding='utf-8') as SCRIPT_HTTT_file:
-    text_copy = SCRIPT_HTTT_file.read()
+while True: 
+    #Input Page name
+    Page_name = input("Please input page name: ")
+    #Read the script
+    with open("SCRIPT_HTTT.txt", encoding='utf-8') as SCRIPT_HTTT_file:
+        text_copy = SCRIPT_HTTT_file.read()
 
-#Replace the Page's name
-text_copy = text_copy.replace("{}", Page_name)
-text_copy = text_copy.replace("(TÊN PAGE)", Page_name)
-text_copy = text_copy.replace("(PAGE'S NAME)", Page_name)
-text_copy = text_copy.replace("(PAGE's NAME)", Page_name)
+    #Replace the Page's name
+    for replace_key in List_replace_Key:
+        text_copy = text_copy.replace(replace_key, Page_name)
 
-#Check and Copy into clipboard
-print(text_copy)
-pyperclip.copy(text_copy)
-spam = pyperclip.paste()
+    #Check and Copy into clipboard
+    print("Text copied into clipboard! Enjoy!")
+    pyperclip.copy(text_copy)
+    spam = pyperclip.paste()
